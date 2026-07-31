98fd47b374b242fdae4ce46f7fc622f543fbf7c0
Anurag <code.annu@gmail.com>
2026-07-31 16:59:44 +0530

    add movie test cases and seed script

--- backend/test/helpers/cleanup.ts
+++ backend/test/helpers/cleanup.ts
@@ -4,4 +4,6 @@ export async function resetDb() {
   await prisma.profile.deleteMany();
   await prisma.session.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.movie.deleteMany();
+  await prisma.theater.deleteMany();
 }
