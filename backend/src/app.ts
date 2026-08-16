8fb09690e3311cbbde13f4a0411da845a3c4614f
Anurag <code.annu@gmail.com>
2026-08-16 18:13:29 +0530

    Implement movie, theater and show feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,9 @@ import handleError from "./shared/middleware/error-handler.middleware";
 import container from "./core/di/inversify.config";
 import TYPES from "./core/di/inversify.types";
 import AuthRouter from "./modules/auth/auth.router";
+import MovieRouter from "./modules/movie/movie.router";
+import TheaterRouter from "./modules/theater/theater.router";
+import ShowRouter from "./modules/show/show.router";
 
 const app = express();
 
@@ -13,6 +16,18 @@ app.use(cookieParser());
 // Auth routes
 const authRouter = container.get<AuthRouter>(TYPES.AuthRouter);
 app.use("/api/auth", authRouter.router);
+
+// Movie routes
+const movieRouter = container.get<MovieRouter>(TYPES.MovieRouter);
+app.use("/api/movies", movieRouter.router);
+
+// Theater routes
+const theaterRouter = container.get<TheaterRouter>(TYPES.TheaterRouter);
+app.use("/api/theaters", theaterRouter.router);
+
+// Show routes
+const showRouter = container.get<ShowRouter>(TYPES.ShowRouter);
+app.use("/api/shows", showRouter.router);
 
 app.use(handleError);
 
--- backend/src/core/prisma/prisma.client.ts
+++ backend/src/core/prisma/prisma.client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { Prisma, PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import ENV from "../config/env";
@@ -7,5 +7,6 @@ const pool = new pg.Pool({ connectionString: ENV.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
-export type PrismaTx = Parameters<Parameters<typeof prisma.$transaction>[0]>[0];
+// export type PrismaTx = Parameters<Parameters<typeof prisma.$transaction>[0]>[0];
 export { prisma };
+export type Tx = Prisma.TransactionClient;
